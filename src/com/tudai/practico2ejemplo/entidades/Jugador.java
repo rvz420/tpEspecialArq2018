73217c0e617d11a118b92b053164554dbbd04c64
BoulangerNieto <nfseba@gmail.com>
2018-09-17 16:23:01 -0300

    primer commit, ejemplo base de la catedra

---
+++ src/com/tudai/practico2ejemplo/entidades/Jugador.java
@@ -0,0 +1,14 @@
+package com.tudai.practico2ejemplo.entidades;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+
+public class Jugador implements Serializable {
+
+}
